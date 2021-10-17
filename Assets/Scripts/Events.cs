dbf95deb447e1eca47875c0b84653251f5619cf6
kristensurva <kristensurva@gmail.com>
2021-10-17 14:59:57 +0300

    Added an event for turn passing

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -2,26 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-/// <summary> All of the code here is placeholder, keeping it so I know how to start when we need to make events. </summary>
 /// More on events - https://www.youtube.com/watch?v=k4JlFxPcqlg
 /// C# events or unity events? - https://www.youtube.com/watch?v=oc3sQamIh-Q
 public static class Events
 {
-    public static event Action<int> OnSetScore;
-
-    public static void SetScore(int value) => OnSetScore?.Invoke(value); //OnSetScore? checks if null or not
-
-    public static event Func<int> OnRequestScore;
-    public static int RequestScore() => OnRequestScore?.Invoke() ?? 0; //?? 0 default value
-
-
-    // - Or alternatively -
-    /*public static void SetScore(int value)
-    //{
-        if (OnSetScore!=null) {
-
-        }
-        //OnSetScore.Invoke();
-    //} */
-
+        // Event to invoke everytime a turn passes.
+        public static event Action OnTurnPass;
+        public static void TurnPass() => OnTurnPass.Invoke();
 }

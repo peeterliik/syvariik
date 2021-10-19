eca26cf804214a230c43abfefff0cf989cb2d126
kristensurva <kristensurva@gmail.com>
2021-10-20 00:21:43 +0300

    Removed placeholder events

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -6,27 +6,4 @@ using TMPro;
 public class TurnManager : MonoBehaviour
 {
     private int score = 1;
-
-    private void Awake()
-    {
-        Events.OnSetScore += OnSetScore; //Subscribing method to event
-        Events.OnRequestScore += OnRequestScore;
-    }
-
-    private void OnDestroy()
-    {
-        Events.OnSetScore -= OnSetScore; //Unsubscribing method from event
-        Events.OnRequestScore -= OnRequestScore;
-    }
-
-    private void OnSetScore(int amount)
-    {
-        score = amount;
-    }
-
-    private int OnRequestScore()
-    {
-        return score;
-    }
-
 }
